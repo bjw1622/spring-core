8257fa6a449a72c22392744a6375a4c4eb8e46cf
백재원 <bjw1622@gmail.com>
2023-08-31 09:52:06 +0900

    :sparkles: feat : Discount, Order domain 추가

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -3,5 +3,11 @@ package hello.core.discount;
 import hello.core.member.Member;
 
 public interface DiscountPolicy {
+    /**
+     *
+     * @param member
+     * @param pirce
+     * @return 할인 대상 금액
+     */
     int discount(Member member, int pirce);
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,7 +7,9 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepositroy;
 
 public class OrderServiceImpl implements OrderService {
+    // 회원 찾기
     private final MemberRepository memberRepository = new MemoryMemberRepositroy();
+    // 고정 할인 정책
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     @Override
@@ -15,6 +17,7 @@ public class OrderServiceImpl implements OrderService {
         Member member = memberRepository.findById(memberId);
         /*
         *   단일 체계 원칙을 잘 지킨 예시
+        *   OrderService 입장에서는 할인 정책에 대해서 전혀 알지 못함
         *   만약 할인 정책이 바뀌더라도 실질적으로 OrderService에서 바뀌는 값은 없음
         * */
         int discountPrice = discountPolicy.discount(member, itemPrice);
