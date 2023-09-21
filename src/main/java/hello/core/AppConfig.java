b89105afc871ec598cfb697a7ea15103b6577c3f
백재원 <bjw1622@gmail.com>
2023-09-21 14:54:35 +0900

    :sparkles: feat : OrderApp 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepositroy;
@@ -9,13 +11,22 @@ import hello.core.order.OrderServiceImpl;
 
 // 나의 application 전체를 설정하고 구성
 public class AppConfig {
+    // 생성과 동시에 생성자로 전달
+    // MemberSercieImpl 입장에서는 외부에서(appConfig)가 의존 관계를 주입하는 것 처럼
     public MemberService memberService() {
-        // 생성과 동시에 생성자로 전달
-        // MemberSercieImpl 입장에서는 외부에서(appConfig)가 의존 관계를 주입하는 것 처럼
-        return new MemberServiceImpl(new MemoryMemberRepositroy());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepositroy memberRepository() {
+        return new MemoryMemberRepositroy();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepositroy(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
