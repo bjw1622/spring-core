f0df2e9ef2602b79edfe1b6747adcccf3dacd553
백재원 <bjw1622@gmail.com>
2023-09-21 12:27:27 +0900

    :sparkles: feat : discount Test 수정

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -24,7 +24,7 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
     void vip_x(){
         //given
-        Member member = new Member(1L, "memberVIP", Grade.BASIC);
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
